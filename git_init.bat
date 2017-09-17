c92946190f6bc9c71a441c099efb88bf6a02bb93
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 14:04:40 -0400

    Removed old setup scripts

--- git_init.bat
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin git@github.com:bincrafters/conan-boost-serialization.git
-git fetch
-git add .
-git commit -m "initial commit"
-git push --set-upstream origin master
--- upload.sh
+++
@@ -1,1 +0,0 @@
-conan upload --all --remote bincrafters Boost.Serialization/1.65.1@bincrafters/testing
