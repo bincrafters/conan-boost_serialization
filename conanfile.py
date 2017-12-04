cffd25453bb1b2e9e2c4f7b2bc93fc9625289555
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:30 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,30 @@
-from conans import ConanFile
+from conans import ConanFile, tools
 
 
 class BoostSerializationConan(ConanFile):
     name = "Boost.Serialization"
     version = "1.65.1"
+    requires = "Boost.Level11Group/1.65.1@bincrafters/testing"
+    lib_short_names = ["serialization"]
+    is_header_only = False
+    is_in_cycle_group = True
+
+    # BEGIN
+
     url = "https://github.com/bincrafters/conan-boost-serialization"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    requires =  "Boost.Level11Group/1.65.1@bincrafters/testing"
-   
-    def package_id(self):
-        self.info.header_only()
-        
-    #This library is part of one or more cyclic dependency groups within Boost.
-    
-    #All members of cyclic dependency groups must be built under single package per group for Conan.
-    
-    #The combination is performed in the package(s) listed in the requires field.
-    
-    #This package enables simple consumption of this library while abstracting away the cyclic dependency issues. 
+    short_paths = True
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator  # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
+
+    # END
