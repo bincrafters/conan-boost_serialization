4c84b4f91dab1307c9abc6d4a81271011250b373
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:49 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,9 @@ from conans import ConanFile, tools
 class BoostSerializationConan(ConanFile):
     name = "Boost.Serialization"
     version = "1.65.1"
-    requires = "Boost.Level11Group/1.65.1@bincrafters/testing"
+    requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
+        "Boost.Level11Group/1.65.1@bincrafters/testing"
     lib_short_names = ["serialization"]
     is_header_only = False
     is_in_cycle_group = True
@@ -12,10 +14,12 @@ class BoostSerializationConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-serialization"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def env(self):
