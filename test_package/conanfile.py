cf098b81e95bbf8df0781d0e688370edce090840
Rene Rivera <grafikrobot@gmail.com>
2018-04-20 15:55:57 -0500

    Templated test_package [skip ci]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,14 +9,19 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def requirements(self):
+        boost_deps = ['array', 'assert', 'config', 'core', 'detail', 'integer', 'io', 'iterator', 'move', 'mpl', 'optional', 'predef', 'preprocessor', 'serialization', 'smart_ptr', 'spirit', 'static_assert', 'type_traits', 'unordered', 'utility', 'variant']
+        for lib in boost_deps:
+            self.requires("boost_" + lib + "/1.67.0@" + self.user + "/" + self.channel)
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        
+
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
             if self.settings.os == "Windows":
-                self.run(os.path.join("bin","test_package"))
+                self.run(os.path.join("bin", "test_package"))
             else:
-                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ['DYLD_LIBRARY_PATH'],os.path.join("bin","test_package")))
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ['DYLD_LIBRARY_PATH'], os.path.join("bin", "test_package")))
