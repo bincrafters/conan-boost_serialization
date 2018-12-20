db3fa700757485e1dc5d134beb9642dc7c84f0d6
Jerry W <jerrywiltse@gmail.com>
2018-12-20 02:13:57 -0500

    switch base dep to stable

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/1.69.0@bincrafters/stable")
 
 class BoostSerializationConan(base.BoostBaseConan):
     name = "boost_serialization"
