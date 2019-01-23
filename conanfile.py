70b4bc1641c1726d2b40d0f289b05f923c2a9de8
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:19 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostSerializationConan(base.BoostBaseConan):
     name = "boost_serialization"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_serialization"
     lib_short_names = ["serialization"]
     cycle_group = "boost_level11group"
